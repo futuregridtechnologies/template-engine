93c5593e3e9436e5ab37fd0971676dbdc59eac7a
mmg0311 <mohakgadge@gmail.com>
2020-06-11 18:26:49 +0530

    fix: fixed the label templates and added the get recipe query

--- templates/recipeCards/moonlight/index.js
+++ templates/recipeCards/moonlight/index.js
@@ -1,5 +1,4 @@
 import { client } from '../../../lib/graphql'
-import { GET_RECIPE } from '../../../graphql/recipe'
 
 const fs = require("fs");
 const path = require("path");
@@ -10,6 +9,35 @@ const readFile = utils.promisify(fs.readFile)
 
 const filepath = path.join(__dirname, '/a4.html');
 
+const GET_RECIPE = `
+   query MyQuery($id:INT!) {
+      orderMealKitProduct(id: $id) {
+        simpleRecipeProduct {
+          name
+          simpleRecipe {
+            author
+            cookingTime
+            cuisine
+            image
+            procedures
+            type
+            utensils
+          }
+          tags
+        }
+        simpleRecipeProductOption {
+          simpleRecipeYield {
+            yield
+            ingredientSachets {
+              slipName
+            }
+          }
+        }
+      }
+    }
+`
+
+
 export const createMoonlightHTML = async function (request) {
    const htmlInput = await client.request(GET_RECIPE, { id: request.id })
    handlebars.registerHelper('json', function (context) {
@@ -17,6 +45,31 @@ export const createMoonlightHTML = async function (request) {
    });
    const templateHtml = fs.readFileSync(filepath, 'utf8');
    const template = handlebars.compile(templateHtml);
-   const html = template(htmlInput.simpleRecipe);
+   const html = template(htmlInput.orderMealKitProduct);
    return html;
 }
+
+
+ // query simpleRecipe($id: Int!) {
+   //    simpleRecipe(id: $id) {
+   //       name
+   //       utensils
+   //       procedures
+   //       image
+   //       description
+   //       cuisine
+   //       cookingTime
+   //       author
+   //       simpleRecipeYields {
+   //         ingredientSachets {
+   //           ingredientSachet {
+   //             ingredient {
+   //               image
+   //             }
+   //           }
+   //           slipName
+   //           isVisible
+   //         }
+   //       }
+   //     }  
+   // }
